c9381f4e05010943ffce7af6e8266077da8751ed
Joe Shepherd <joeshepmedia@gmail.com>
2020-12-04 15:08:57 -0600

    chapter 5

--- scripts/convictions/ConvictionSelect.js
+++ scripts/convictions/ConvictionSelect.js
@@ -2,10 +2,31 @@
  *   ConvictionSelect component that renders a select HTML element
  *   which lists all convictions in the Glassdale PD API
  */
+// listen for the selection of a crime and capture the chosen value
+// send out a message ( customEvent) via the eventHub
+
 import { useConvictions, getConvictions } from "./ConvictionProvider.js"
 
 // Get a reference to the DOM element where the <select> will be rendered
 const contentTarget = document.querySelector(".filters__crime")
+const eventHub = document.querySelector(".container")
+
+eventHub.addEventListener("change", event => {
+
+  // Only do this if the `crimeSelect` element was changed
+  if (event.target.id === "crimeSelect") {
+      // Create custom event. Provide an appropriate name.
+      const customEvent = new CustomEvent("crimeChosen", {
+          detail: {
+              crimeThatWasChosen: event.target.value
+          }
+      })
+
+      // Dispatch to event hub
+      eventHub.dispatchEvent(customEvent)
+  }
+})
+
 
 export const ConvictionSelect = () => {
     // Trigger fetching the API data and then loading it into application state
@@ -28,7 +49,7 @@ const render = convictionsCollection => {
             <option value="0">Please select a crime...</option>
             ${
                 convictionsCollection.map((crime) => `
-                  <option>
+                  <option value=${crime.id}>
                     ${crime.name}
                   </option>
                 `)
